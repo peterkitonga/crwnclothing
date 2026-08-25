4b5f07cd6fc2846318f379d05061fdf70d485e52
Peter Kitonga <pkitonga.pk@gmail.com>
2026-08-25 19:33:36 +0300

    Add a navigation component

---
+++ src/layouts/navigation/navigation.component.tsx
@@ -0,0 +1,23 @@
+import {Fragment} from 'react';
+import {Link, Outlet} from 'react-router-dom';
+
+import './navigation.styles.scss';
+import { ReactComponent as CrwnLogo } from '../../assets/images/crown.svg';
+
+export default function Navigation() {
+  return (
+    <Fragment>
+      <nav className={'navigation'}>
+        <Link to={'/'} className={'logo-container'}>
+          <CrwnLogo className={'logo'} />
+        </Link>
+        <div className={'nav-links-container'}>
+          <Link to={'/shop'} className={'nav-link'}>
+            Shop
+          </Link>
+        </div>
+      </nav>
+      <Outlet />
+    </Fragment>
+  )
+}
