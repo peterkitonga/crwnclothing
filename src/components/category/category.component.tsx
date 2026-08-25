d394f9456bef1d5f78dd9dbb9a28dd420b3f635d
Peter Kitonga <pkitonga.pk@gmail.com>
2026-08-25 14:24:27 +0300

    Create a category component

---
+++ src/components/category/category.component.tsx
@@ -0,0 +1,16 @@
+import './category.styles.scss';
+import {CategoryItem} from '../../types/interfaces';
+
+export default function Category(props: { category: CategoryItem }) {
+  const { id, title, imageUrl } = props.category;
+
+  return (
+    <div key={id} className={'category-container'}>
+      <div className={'category-background-img'} style={{ backgroundImage: `url(${imageUrl})` }}></div>
+      <div className={'category-body-container'}>
+        <h2>{title.toUpperCase()}</h2>
+        <p>Shop Now</p>
+      </div>
+    </div>
+  )
+}
