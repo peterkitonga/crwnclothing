98b3ffe21e251fcc771daf7e42b3291cf206f746
Peter Kitonga <pkitonga.pk@gmail.com>
2026-09-04 17:21:15 +0300

    Implement the sign up & sign in forms in the auth page

--- src/pages/auth/auth.component.tsx
+++ src/pages/auth/auth.component.tsx
@@ -1,5 +1,13 @@
 import './auth.styles.scss';
+import SignInForm from '@components/sign-in-form/sign-in-form.component';
+import SignUpForm from '@components/sign-up-form/sign-up-form.component';
 
 export default function Auth() {
-  return <main></main>;
+  return (
+    <main className={'auth-container'}>
+      <SignInForm />
+
+      <SignUpForm />
+    </main>
+  );
 }
