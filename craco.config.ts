588cdf5e2a773e7ada0b9d065b1920ae995d4f4e
Peter Kitonga <pkitonga.pk@gmail.com>
2026-09-11 20:08:53 +0300

    Add path alias for custom hooks folder

--- craco.config.ts
+++ craco.config.ts
@@ -6,6 +6,7 @@ module.exports = {
       '@assets': path.resolve(__dirname, './src/assets'),
       '@components': path.resolve(__dirname, './src/components'),
       '@contexts': path.resolve(__dirname, './src/contexts'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
       '@layouts': path.resolve(__dirname, './src/layouts'),
       '@models': path.resolve(__dirname, './src/models'),
       '@pages': path.resolve(__dirname, './src/pages'),
