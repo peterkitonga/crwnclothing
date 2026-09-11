bd74ef25856cdea4cd3456f873d4b91c40c03e15
Peter Kitonga <pkitonga.pk@gmail.com>
2026-09-11 20:10:42 +0300

    Add custom hook for listening to outside clicks

---
+++ src/hooks/click-outside.hook.tsx
@@ -0,0 +1,21 @@
+import { RefObject, useEffect, useRef } from 'react';
+
+export function useClickOutside(callback: () => void): RefObject<HTMLDivElement | null> {
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClick = (event: MouseEvent) => {
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        callback();
+      }
+    };
+
+    document.addEventListener('mousedown', handleClick);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClick);
+    };
+  }, [callback]);
+
+  return ref;
+}
