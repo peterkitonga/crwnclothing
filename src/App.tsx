e646f513cd417292541a42dd2e8f08474e91721f
Peter Kitonga <pkitonga.pk@gmail.com>
2026-09-11 19:14:19 +0300

    Add route for the checkout page

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Navigation from '@layouts/navigation/navigation.component';
 import Home from '@pages/home/home.component';
 import Shop from '@pages/shop/shop.component';
 import Auth from '@pages/auth/auth.component';
+import Checkout from '@pages/checkout/checkout.component';
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
         <Route index element={<Home />}></Route>
         <Route path={'shop'} element={<Shop />}></Route>
         <Route path={'auth'} element={<Auth />}></Route>
+        <Route path={'checkout'} element={<Checkout />}></Route>
       </Route>
     </Routes>
   );
